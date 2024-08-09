5a700280a3f3f3c374928bcab10d06e424c82866
Prince Kumar <princekumar201926@gmail.com>
2024-08-09 22:38:41 +0530

    09

---
+++ frontend/src/components/ProfileBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfileBox = () => {
+
+  return (
+    <div className="card w-60 h-72 flex flex-row bg-slate-500">
+      
+    </div>
+  );
+};
+
+export default ProfileBox;
