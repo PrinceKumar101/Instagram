0e04fbae1afafd2e3a82f010d05d199f5d7e83fe
Prince Kumar <princekumar201926@gmail.com>
2024-08-15 22:45:17 +0530

    15

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import Topbar from "./Topbar";
+import Bottombar from "./Bottombar";
 
 const MainLayout = () => {
+  console.log("Mainlayout");
+
   return (
     <>
-      <div className="flex gap-3 bg-primary_color">
-        <div className="w-1/6 bg-slate-50 h-full fixed ">
+      <div className="hidden gap-3 bg-primary_color md:flex ">
+        <header className="w-1/6 bg-slate-50 h-full sticky top-0 ">
           <Sidebar />
-        </div>
-        <div className="w-full flex flex-col items-center justify-center pt-5">
+        </header>
+        <main className="w-full flex flex-col pt-5 overflow-x-hidden">
           <Outlet />
-        </div>
+        </main>
+      </div>
+
+      <div className="flex flex-col h-full md:hidden">
+        <header className="fixed top-0 left-0 right-0 w-full z-10">
+          <Topbar />
+        </header>
+        <main className="flex-grow overflow-y-auto pt-16 pb-16">
+          <Outlet />
+        </main>
+        <footer className="fixed bottom-0 left-0 right-0 w-full z-10">
+          <Bottombar />
+        </footer>
       </div>
     </>
   );
