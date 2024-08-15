c8bd83a54849a64b8430ac7f2315a9ce03e635bc
Prince Kumar <princekumar201926@gmail.com>
2024-08-15 22:44:54 +0530

    15

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios_setup from "@/assets/Axios";
+import React, { useEffect, useState } from "react";
 
 const Message = () => {
-  return (
-    <div>Message</div>
-  )
-}
+  const [data, setdata] = useState();
+  const [error, seterror] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios_setup.get("/message");
+        setdata(res.data);
+        console.log("Data fetched:", res.data);
+      } catch (error) {
+        seterror(error);
+        console.error("Error fetching data:", error);
+      }
+      console.log("Fetch completed");
+    };
 
-export default Message
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <>
+      <div>{data ? data.message : "loading"}</div>
+      <div>{error}</div>
+    </>
+  );
+};
+
+export default Message;
