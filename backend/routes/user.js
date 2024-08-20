0b6bf58357d405aef6475c1f49af4527a01b42f6
Prince Kumar <princekumar201926@gmail.com>
2024-08-20 23:58:06 +0530

    20

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { register } = require("../controllers/user.controller");
+const isLoggedIn = require("../middleware/isLoggedin");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
@@ -7,6 +9,13 @@ router.get("/", function (req, res, next) {
 
 router.get("/message", function (req, res, next) {
   res.json({ message: "heyy got it at message" });
+});
+
+router.post("/signup", register);
+
+router.get("/profile", isLoggedIn, async function (req, res, next) {
+  const found_user = await userModel.findOne({email: req.user.email});
+  res.json({found_user});
 });
 
 module.exports = router;
